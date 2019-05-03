fd6a11e9ce1717fe84af13150e24ca553101b368
Murabac <mur7bac@gmail.com>
2019-05-03 10:34:26 +0300

    checkpoint

--- apps/auth/models/artist.py
+++ apps/auth/models/artist.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import AbstractUser
 class Artist(AbstractUser):
     class Meta:
         db_table = 'auth_user'
-
-    pass
-
+        name =  models.CharField(max_length = 50)
+        email_address = models.CharField(max_length = 50)
+        password = models.CharField(max_length = 255)
+        phone_number = models.CharField(max_length = 50)
+        address =  models.CharField(max_length = 50)
+        biography = models.CharField(max_length = 255)
+        profile_picture_path = models.ImageField()
+        twitter = models.CharField(max_length = 50 )
+        instagram = models.CharField(max_length = 50)
+        facebook = models.CharField(max_length = 50)
+        gender = models.BooleanField()
+        rating =  models.DecimalField()
