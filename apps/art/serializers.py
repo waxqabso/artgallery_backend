7f7f71e0fe8d5890b128e5c2a6a6fe762bfdc3c6
= <nick@innomadic.com>
2019-05-03 15:27:42 +0300

    Added availability to serializer, added pagination

--- apps/art/serializers.py
+++ apps/art/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class ArtSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Art
-        fields = ('name', 'description', 'created', 'rating', 'image','timeframe_for_order', 'price')
+        fields = ('name', 'description', 'created', 'rating', 'image','timeframe_for_order', 'price', 'availability')
--- settings/settings.py
+++ settings/settings.py
@@ -132,3 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+## ReST framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 30
+}
