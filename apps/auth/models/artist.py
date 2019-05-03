58b04ea942c6cce49cfd45df3a67b39085a9cfb1
Murabac <mur7bac@gmail.com>
2019-05-03 12:33:44 +0300

    Artist Model

--- apps/auth/models/artist.py
+++ apps/auth/models/artist.py
@@ -2,17 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Artist(AbstractUser):
+
+    name =  models.CharField(max_length = 50)
+    email_address = models.EmailField(max_length = 254)
+    password = models.CharField(max_length = 255)
+    phone_number = models.PhoneNumberField()
+    address =  models.TextField(max_length = 50 , blank = True)
+    biography = models.TextField(blank = True)
+    profile_picture_path = models.ImageField()
+    twitter = models.URLField(max_length = 50 , blank = True)
+    instagram = models.URLField(max_length = 50, blank = True)
+    facebook = models.URLField(max_length = 50, blank = True)
+    gender = models.BooleanField()
+    rating =  models.IntegerField()
+
     class Meta:
         db_table = 'auth_user'
-        name =  models.CharField(max_length = 50)
-        email_address = models.EmailField(max_length = 254)
-        password = models.CharField(max_length = 255)
-        phone_number = models.PhoneNumberField()
-        address =  models.CharField(max_length = 50)
-        biography = models.TextField()
-        profile_picture_path = models.ImageField()
-        twitter = models.CharField(max_length = 50 )
-        instagram = models.CharField(max_length = 50)
-        facebook = models.CharField(max_length = 50)
-        gender = models.BooleanField()
-        rating =  models.DecimalField()
