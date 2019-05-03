173ea09b42dbde268781eed7eb0dc19f00296c0a
= <nick@innomadic.com>
2019-05-03 10:31:58 +0300

    Draft of Art Model

--- apps/art/models/art.py
+++ apps/art/models/art.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Art(models.Model):
+
+    class Availability:
+        AVAILABLE_NOW = 'AN'
+        AVAILABLE_LATER = 'AL'
+        NOT_AVAILABLE = 'NA'
+        CHOICES = {
+            (AVAILABLE_NOW = 'Available Now')
+            (AVAILABLE_LATER = 'Available for Order')
+            (NOT_AVAILABLE = 'Not Available')
+        }
+
+    availability = models.CharField(
+        max_length=2,
+        choices=Availability.CHOICES,
+        default=AVAILABLE_NOW,
+    )
+
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_new_add=True)
+    image = models.ImageField()
+    rating = models.IntegerField(default=0)
+    artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    timeframe_for_order = models.CharField(max_length=200)
+    price = models.DecimalField(decimal_places=2)
