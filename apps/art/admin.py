1bd4e0711cb09dbfb344f19b33c9811aace4690b
= <nick@innomadic.com>
2019-05-03 11:24:15 +0300

    Created Art model

--- apps/art/admin.py
+++ apps/art/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Art
+
 # Register your models here.
+
+admin.site.register(Art)
+#admin.site.register(Artist)
--- apps/art/models/art.py
+++ apps/art/models/art.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+MEDIA_UPLOAD_PATH = './media'
+
 class Art(models.Model):
 
     class Availability:
@@ -9,22 +11,22 @@ class Art(models.Model):
         AVAILABLE_LATER = 'AL'
         NOT_AVAILABLE = 'NA'
         CHOICES = {
-            (AVAILABLE_NOW = 'Available Now')
-            (AVAILABLE_LATER = 'Available for Order')
-            (NOT_AVAILABLE = 'Not Available')
+            (AVAILABLE_NOW, 'Available Now'),
+            (AVAILABLE_LATER, 'Available for Order'),
+            (NOT_AVAILABLE, 'Not Available'),
         }
 
     availability = models.CharField(
         max_length=2,
         choices=Availability.CHOICES,
-        default=AVAILABLE_NOW,
+        default=Availability.AVAILABLE_NOW,
     )
 
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
-    created_at = models.DateTimeField(auto_new_add=True)
-    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=MEDIA_UPLOAD_PATH)
     rating = models.IntegerField(default=0)
-    artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    timeframe_for_order = models.CharField(max_length=200)
-    price = models.DecimalField(decimal_places=2)
+    #artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    timeframe_for_order = models.CharField(max_length=200, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=15)
